bcb30c1baeb2d0daef0ef3509ec548f7bd411b5d
Felipe Borges de Oliveira Veríssimo <felbrou@gmail.com>
2021-05-27 20:43:38 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,3 +1,5 @@
+#   Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
 salario = float(input('Saiba seu salário com o valor de 15% de acréscimo R$:'))
 novo_salario = salario + (salario*15/100)
 print('Seu salário de R${:.2f} com aumento de 15% ficará R${:.2f}'.format(salario, novo_salario))
