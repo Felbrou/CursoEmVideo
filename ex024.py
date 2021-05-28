509e4ab56a1207c94c8400c6f9b5cbdd6bb19bfc
Felipe Borges de Oliveira Veríssimo <felbrou@gmail.com>
2021-05-27 21:27:59 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -2,5 +2,5 @@
 #   Faça um programa que leia o nome de uma cidade
 #   e diga se ela começa ou não com o nome "Santos".
 
-cidade = str(input('Informe o nome de alguma cidade:')).strip()
-print(cidade[:5] == 'Santo')
+cidade = str(input('Informe o nome da sua cidade:')).strip()
+print(cidade[:5].upper() == 'SANTO')
