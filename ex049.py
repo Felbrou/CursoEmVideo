96d10dcab1aff1d42786ef14a882dba0e479bcd7
felbrou <felbrou@gmail.com>
2021-12-05 11:36:23 -0300

    add a number variable in the string add comments

--- ex049.py
+++ ex049.py
@@ -1,8 +1,8 @@
 #Python Exercise 49: Redo CHALLENGE 9, showing the times tables of a number the user chooses, only now using a for loop.
-f1 = int(input('Enter a number and discover your multiplication table: '))
+f1 = int(input('Enter a number and discover your multiplication table: ')) #first catch the first factor
 print('-'*32)
-print("\033[035mThe multiplication table of {} is:\033[m".format(f1))
+print("\033[035mThe multiplication table of {} is:\033[m".format(f1))#showing the multiplication table of factor one
 print('-'*32)
-for f2 in range (0,11):
-    print("\033[032m{} x {}\033[m = \033[034m{}\033[m".format(f1,f2,f1 * f2))
+for f2 in range (0,11): #a loop for factor 2
+    print("\033[032m{} x {}\033[m = \033[034m{}\033[m".format(f1,f2,f1 * f2))#here the output of the multiplication table
 
