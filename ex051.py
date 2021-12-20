3f2de85d177eda5837db03e8710352277fb642b0
felbrou <Felbrou@users.noreply.github.com>
2021-12-19 22:40:41 -0300

    erros concertados

--- ex051.py
+++ ex051.py
@@ -6,7 +6,7 @@ a_tenth = a_first + (10 - 1) * r
 
 print('The Arithmetic Progression of \033[034m{}\033[m is:'.format(a_first))
 print('\033[31m-\033[m'*34)
-for var in range ( a_first, a_tenth + r, r):
+for var in range(a_first, a_tenth + r, r):
     print('\033[34m{}\033[m'.format(var), end=' \033[037m->\033[m')
 print('\033[36mDONE!\033[m')
 
